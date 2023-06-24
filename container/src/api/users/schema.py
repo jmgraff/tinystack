3b7d19759b2224fe1fc161a5727b2f2a6429a3b1
John Graff <john@codehammer.io>
2023-06-24 17:26:05 -0400

    T13: Use kubernetes (#14)

--- container/src/api/users/schema.py
+++ container/src/api/users/schema.py
@@ -5,7 +5,7 @@ from ninja import Schema, ModelSchema
 class UserSchema(ModelSchema):
     class Config:
         model = User
-        model_fields = ["id", "username"]
+        model_fields = ["id", "username", "is_staff"]
 
 
 class LoginSchema(Schema):
